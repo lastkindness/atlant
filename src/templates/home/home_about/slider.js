c551831589cd2c0ddb27749413e2776909469c20
lastkindness <lastkindnesss@gmail.com>
2018-04-27 18:11:00 +0300

    add about and sold

---
+++ src/templates/home/home_about/slider.js
@@ -0,0 +1,28 @@
+import $ from '../../../generals/jquery-pligins';
+
+export default class {
+    constructor () {
+        const slider = $('.home_about_item_slider');
+        const sliderFor = $('.home_about_slider-for');
+        const sliderNav = $('.home_about_slider-nav');
+
+        if (slider.length) {
+            sliderFor.slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                fade: true,
+                asNavFor: sliderNav
+            });
+            sliderNav.slick({
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                asNavFor: sliderFor,
+                arrows: false,
+                dots: false,
+                centerMode: true,
+                focusOnSelect: true
+            });
+        }
+    }
+}
--- src/templates/home/index.js
+++ src/templates/home/index.js
@@ -1,5 +1,7 @@
 import SliderTop from './home_slider-top/slider';
+import SliderAbout from './home_about/slider';
 
 export default () => {
     new SliderTop();
+    new SliderAbout();
 }
