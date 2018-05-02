06ef11eeec8e27f06de5b80ae92b864d9b6dbee9
lastkindness <lastkindnesss@gmail.com>
2018-05-02 19:19:41 +0300

    add sale popup and points

--- src/templates/home/home_sale/active.js
+++ src/templates/home/home_sale/active.js
@@ -1,35 +1,45 @@
 export default class {
     constructor () {
+        if(document.readyState === 'interactive') this.init();
+        else document.addEventListener("DOMContentLoaded", this.init.bind(this));
+    }
+
+    init () {
         const elmTrigger = document.querySelectorAll('.home_sale_nav_item');
         const elmCont = document.querySelectorAll('.home_sale_content_item');
         const elmContWrapper = document.querySelector('.home_sale_content');
-        elmTrigger[0].classList.add("active");
-        elmCont[0].classList.add("active");
-        elmCont[0].style.position = 'relative';
-        elmCont[0].style.zIndex = '1';
-        elmContWrapper.style.maxHeight = elmCont[0].querySelector('img').offsetHeight + 'px';
-        for (var i=0; i<elmTrigger.length; i++) {
-            elmTrigger[i].addEventListener("click", function() {
-                elmTrigger.forEach(function(item, i, elmTrigger) {
-                    elmTrigger[i].classList.remove("active");
-                    elmCont[i].classList.remove("active");
+        const firstImage = elmCont[0].querySelector('img');
+        console.log(firstImage);
+        firstImage.onload = function() {
+            console.log(firstImage);
+            elmTrigger[0].classList.add("active");
+            elmCont[0].classList.add("active");
+            elmCont[0].style.position = 'relative';
+            elmCont[0].style.zIndex = '1';
+            elmContWrapper.style.maxHeight = firstImage.offsetHeight + 'px';
+            for (var i=0; i<elmTrigger.length; i++) {
+                elmTrigger[i].addEventListener("click", function() {
+                    elmTrigger.forEach(function(item, i, elmTrigger) {
+                        elmTrigger[i].classList.remove("active");
+                        elmCont[i].classList.remove("active");
+                        setTimeout(function () {
+                            elmCont[i].style.position = 'absolute';
+                            elmCont[i].style.zIndex = '-1';
+                        }, 500);
+                    });
+                    // elmTrigger.indexOf = [].indexOf;
+                    // console.log(elmTrigger.indexOf(this));
+                    var thisIndex = Array.prototype.indexOf.call(elmTrigger, this);
+                    var itemMaxHeight = elmCont[thisIndex].querySelector('img').offsetHeight + 'px';
+                    elmContWrapper.style.maxHeight = itemMaxHeight;
+                    this.classList.add("active");
                     setTimeout(function () {
-                        elmCont[i].style.position = 'absolute';
-                        elmCont[i].style.zIndex = '-1';
+                        elmCont[thisIndex].style.position = 'relative';
+                        elmCont[thisIndex].style.zIndex = '1';
+                        elmCont[thisIndex].classList.add("active");
                     }, 500);
-                });
-                // elmTrigger.indexOf = [].indexOf;
-                // console.log(elmTrigger.indexOf(this));
-                var thisIndex = Array.prototype.indexOf.call(elmTrigger, this);
-                var itemMaxHeight = elmCont[thisIndex].querySelector('img').offsetHeight + 'px';
-                elmContWrapper.style.maxHeight = itemMaxHeight;
-                this.classList.add("active");
-                setTimeout(function () {
-                    elmCont[thisIndex].style.position = 'relative';
-                    elmCont[thisIndex].style.zIndex = '1';
-                    elmCont[thisIndex].classList.add("active");
-                }, 500);
-            }, false);
-        }
+                }, false);
+            };
+        };
     }
 }
---
+++ src/templates/home/home_sale/points.js
@@ -0,0 +1,379 @@
+ export default [
+    [
+        {
+            top: 358,
+            left: 1057,
+            img: "1",
+            title: "Офис № 1",
+            price: "6000",
+            area: "52"
+        },
+        {
+            top: 723,
+            left: 655,
+            img: "2",
+            title: "Офис № 2",
+            price: "5000",
+            area: "42"
+        },
+        {
+            top: 730,
+            left: 537,
+            img: "3",
+            title: "Офис № 3",
+            price: "4000",
+            area: "32"
+        }
+    ],
+    [
+        {
+            top: 359,
+            left: 411,
+            img: "1",
+            title: "Офис № 1",
+            price: "6000",
+            area: "52"
+        }
+    ],
+    [
+        {
+            top: 151,
+            left: 325,
+            img: "1",
+            title: "Офис № 1",
+            price: "6000",
+            area: "52"
+        },
+        {
+            top: 154,
+            left: 786,
+            img: "1",
+            title: "Офис № 1",
+            price: "6000",
+            area: "52"
+        },
+        {
+            top: 514,
+            left: 218,
+            img: "2",
+            title: "Офис № 2",
+            price: "5000",
+            area: "42"
+        },
+        {
+            top: 514,
+            left: 796,
+            img: "3",
+            title: "Офис № 3",
+            price: "4000",
+            area: "32"
+        },
+        {
+            top: 305,
+            left: 461,
+            img: "3",
+            title: "Офис № 3",
+            price: "4000",
+            area: "32"
+        }
+    ],
+    [
+        {
+            top: 159,
+            left: 324,
+            img: "1",
+            title: "Офис № 1",
+            price: "6000",
+            area: "52"
+        },
+        {
+            top: 159,
+            left: 575,
+            img: "2",
+            title: "Офис № 2",
+            price: "5000",
+            area: "42"
+        },
+        {
+            top: 498,
+            left: 325,
+            img: "3",
+            title: "Офис № 3",
+            price: "4000",
+            area: "32"
+        },
+        {
+            top: 491,
+            left: 766,
+            img: "1",
+            title: "Офис № 1",
+            price: "6000",
+            area: "52"
+        },
+        {
+            top: 325,
+            left: 833,
+            img: "2",
+            title: "Офис № 2",
+            price: "5000",
+            area: "42"
+        },
+        {
+            top: 310,
+            left: 547,
+            img: "2",
+            title: "Офис № 2",
+            price: "5000",
+            area: "42"
+        }
+    ],
+    [
+        {
+            top: 252,
+            left: 211,
+            img: "1",
+            title: "Офис № 1",
+            price: "6000",
+            area: "52"
+        },
+        {
+            top: 226,
+            left: 386,
+            img: "2",
+            title: "Офис № 2",
+            price: "5000",
+            area: "42"
+        },
+        {
+            top: 225,
+            left: 544,
+            img: "3",
+            title: "Офис № 3",
+            price: "4000",
+            area: "32"
+        },
+        {
+            top: 240,
+            left: 799,
+            img: "1",
+            title: "Офис № 1",
+            price: "6000",
+            area: "52"
+        },
+        {
+            top: 337,
+            left: 143,
+            img: "2",
+            title: "Офис № 2",
+            price: "5000",
+            area: "42"
+        },
+        {
+            top: 446,
+            left: 207,
+            img: "3",
+            title: "Офис № 3",
+            price: "4000",
+            area: "32"
+        },
+        {
+            top: 390,
+            left: 298,
+            img: "3",
+            title: "Офис № 3",
+            price: "4000",
+            area: "32"
+        },
+        {
+            top: 411,
+            left: 777,
+            img: "3",
+            title: "Офис № 3",
+            price: "4000",
+            area: "32"
+        },
+        {
+            top: 317,
+            left: 985,
+            img: "3",
+            title: "Офис № 3",
+            price: "4000",
+            area: "32"
+        },
+        {
+            top: 324,
+            left: 445,
+            img: "3",
+            title: "Офис № 3",
+            price: "4000",
+            area: "32"
+        }
+    ],
+    [
+        {
+            top: 241,
+            left: 204,
+            img: "1",
+            title: "Офис № 1",
+            price: "6000",
+            area: "52"
+        },
+        {
+            top: 225,
+            left: 419,
+            img: "2",
+            title: "Офис № 2",
+            price: "5000",
+            area: "42"
+        },
+        {
+            top: 226,
+            left: 535,
+            img: "3",
+            title: "Офис № 3",
+            price: "4000",
+            area: "32"
+        },
+        {
+            top: 239,
+            left: 791,
+            img: "1",
+            title: "Офис № 1",
+            price: "6000",
+            area: "52"
+        },
+        {
+            top: 328,
+            left: 139,
+            img: "2",
+            title: "Офис № 2",
+            price: "5000",
+            area: "42"
+        },
+        {
+            top: 465,
+            left: 204,
+            img: "3",
+            title: "Офис № 3",
+            price: "4000",
+            area: "32"
+        },
+        {
+            top: 383,
+            left: 298,
+            img: "3",
+            title: "Офис № 3",
+            price: "4000",
+            area: "32"
+        },
+        {
+            top: 407,
+            left: 776,
+            img: "3",
+            title: "Офис № 3",
+            price: "4000",
+            area: "32"
+        },
+        {
+            top: 314,
+            left: 1002,
+            img: "3",
+            title: "Офис № 3",
+            price: "4000",
+            area: "32"
+        },
+        {
+            top: 320,
+            left: 441,
+            img: "3",
+            title: "Офис № 3",
+            price: "4000",
+            area: "32"
+        }
+    ],
+    [
+        {
+            top: 209,
+            left: 436,
+            img: "1",
+            title: "Офис № 1",
+            price: "6000",
+            area: "52"
+        },
+        {
+            top: 457,
+            left: 362,
+            img: "2",
+            title: "Офис № 2",
+            price: "5000",
+            area: "42"
+        },
+        {
+            top: 309,
+            left: 538,
+            img: "3",
+            title: "Офис № 3",
+            price: "4000",
+            area: "32"
+        },
+        {
+            top: 587,
+            left: 549,
+            img: "1",
+            title: "Офис № 1",
+            price: "6000",
+            area: "52"
+        },
+        {
+            top: 362,
+            left: 456,
+            img: "2",
+            title: "Офис № 2",
+            price: "5000",
+            area: "42"
+        }
+    ],
+    [
+        {
+            top: 174,
+            left: 162,
+            img: "1",
+            title: "Офис № 1",
+            price: "6000",
+            area: "52"
+        },
+        {
+            top: 663,
+            left: 171,
+            img: "2",
+            title: "Офис № 2",
+            price: "5000",
+            area: "42"
+        },
+        {
+            top: 815,
+            left: 1030,
+            img: "3",
+            title: "Офис № 3",
+            price: "4000",
+            area: "32"
+        },
+        {
+            top: 444,
+            left: 602,
+            img: "1",
+            title: "Офис № 1",
+            price: "6000",
+            area: "52"
+        },
+        {
+            top: 419,
+            left: 171,
+            img: "2",
+            title: "Офис № 2",
+            price: "5000",
+            area: "42"
+        }
+    ]
+
+]
